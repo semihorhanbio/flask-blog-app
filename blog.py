1d372ec729e4485950432f3f3c3289385ec89942
Semih Orhan <sorhan3.14@gmail.com>
2023-02-17 12:53:56 +0300

    finish login form

--- blog.py
+++ blog.py
@@ -19,9 +19,19 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    status_code = 200
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+                request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again.'
+            status_code = 401
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error), status_code
 
 @app.route('/main')
 def main():
